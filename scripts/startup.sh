c540e075dd28ae52b33af07ed2c03036acf25b31
ravish27 <er.ravishsehgal@gmail.com>
2020-08-04 11:16:34 +0530

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,5 +6,5 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y --assume-yes
-sudo docker pull ravish27/cicd-pipeline:5.0.0
-sudo docker run -p 7011:8081 -d -t ravish27/cicd-pipeline:5.0.0
+sudo docker pull ravish27/cicd-pipeline:$1
+sudo docker run -p 7011:8081 -d -t ravish27/cicd-pipeline:$1
